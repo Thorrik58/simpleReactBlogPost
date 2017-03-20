73f36af14a40ad02a409a584a0819d2180f1b173
Þorvarðu Örn Einarsson <thorrik58@gmail.com>
2017-03-20 22:09:45 +0000

    Can now submit a new post with the api

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
 export const FETCH_POST = 'FETCH_POST';
+export const CREATE_POST = 'CREATE_POST'
+
 import { API_KEY } from '../../config'
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
@@ -10,6 +12,15 @@ export function fetchPosts() {
 
   return {
     type: FETCH_POST,
+    payload: request
+  };
+}
+
+
+export function createPost(props){
+  const request = axios.post(`${ROOT_URL}/posts${API_KEY}`, props)
+  return {
+    type: CREATE_POST,
     payload: request
   };
 }
